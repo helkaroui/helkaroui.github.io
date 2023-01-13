2516b582c1b1e9a34305a583542d0f94b1beba5e
helkaroui <helkaroui@users.noreply.github.com>
2023-01-13 19:29:15 +0000

    Auto deploy CICD

---
+++ assets/js/71d09b86.078bef50.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksharek_dev=self.webpackChunksharek_dev||[]).push([[6984],{9301:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/new-tech","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
